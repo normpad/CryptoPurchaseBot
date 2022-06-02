19a8ccefdcef716f5771e0f790f58409213157b5
normpad <normpad48@gmail.com>
2022-06-01 23:12:26 -0400

    Added template config

---
+++ config.py
@@ -0,0 +1,15 @@
+API_KEY = ''
+API_SECRET = ''
+API_PASS = ''
+
+bitcoin_buy = True
+ethereum_buy = True
+cardano_buy = True
+algorand_buy = True
+
+bitcoin_buy_amount = 5 #usd, $5 min
+ethereum_buy_amount = 5 #usd, $5 min
+cardano_buy_amount = 5 #usd, $5 min
+algorand_buy_amount = 5 #usd, $5 min
+
+buy_times = [0] #time of day UTC
